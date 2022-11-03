7a8f6b7a3d13cfe4cecadfa664701d87078c069b
nNabakhteveli <nika.nabakhteveli1@gmail.com>
2022-11-03 15:59:37 +0400

    Add another solution for "First duplicate value"

--- Arrays/Normal/FirstDuplicateValue.cs
+++ Arrays/Normal/FirstDuplicateValue.cs
@@ -5,6 +5,7 @@ using System;
 public class Program
 {
    // Time Complexity: O(n) | Space Complexity: O(1)
+   // Solution without any other DS
    public int FirstDuplicateValue(int[] array)
    {
       int lowestIdxOfDuplicates = -1;
@@ -22,5 +23,22 @@ public class Program
          array[i] = currItem;
       }
       return lowestIdxOfDuplicates == -1 ? -1 : array[lowestIdxOfDuplicates];
+   }
+
+   // Time Complexity: O(n) | Space Complexity: O(n)
+   // Another solution with List
+   public int FirstDuplicateValue(int[] array)
+   {
+      var items = new List<int>();
+
+      foreach (var i in array)
+      {
+         if (items.IndexOf(i) != -1)
+         {
+            return i;
+         }
+         items.Add(i);
+      }
+      return -1;
    }
 }
