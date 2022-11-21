43e37a8f5aa47b1e44f32cd140fc8267dfc8ce7e
nNabakhteveli <nika.nabakhteveli1@gmail.com>
2022-11-21 09:08:18 +0400

    Add time and space complexity for Monotonic array problem

--- Arrays/Normal/MonotonicArray.cs
+++ Arrays/Normal/MonotonicArray.cs
@@ -1,16 +1,17 @@
 using System;
 
 public class Program {
+   // Time Complexity: O(n), Space Complexity: O(1)
 	public static bool IsMonotonic(int[] array) {
 		if (array.Length <= 1) return true;
         
-         int decreasingCount = 0, increasingCount = 0, goal = array.Length - 1;
+         int decreasingCount = 0, increasingCount = 0, streak = array.Length - 1;
          for (int i = 0; i < array.Length; i++) {
             if (i > 0 && array[i] <= array[i - 1]) decreasingCount++;
             
             if (i > 0 && array[i] >= array[i - 1]) increasingCount++;
          }
 
-      return decreasingCount == goal || increasingCount == goal ? true : false;
+      return decreasingCount == streak || increasingCount == streak ? true : false;
 	}
 }
